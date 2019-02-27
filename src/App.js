f6676d81cf4457dce09db37695cf8959bee5a9e6
jonroby <joncaroby@gmail.com>
2019-02-26 21:40:24 -0800

    search component

--- src/App.js
+++ src/App.js
@@ -1,8 +1,35 @@
 import React, { Component } from "react";
 
+class Search extends Component {
+  state = { searchInput: "" };
+
+  updateSearchInput = event => {
+    this.setState({ searchInput: event.target.value }, () => {
+      this.props.history.push(
+        `/search/movies?search-term=${this.state.searchInput}`
+      );
+    });
+  };
+
+  render() {
+    return (
+      <div>
+        <input
+          onChange={this.updateSearchInput}
+          value={this.state.searchInput}
+        />
+      </div>
+    );
+  }
+}
+
 class App extends Component {
   render() {
-    return <div>Webflix Heroku Test</div>;
+    return (
+      <div>
+        <Search history={this.props.history} />
+      </div>
+    );
   }
 }
 
