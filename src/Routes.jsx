561a88e88a81d98313101a00c7244824d6501a78
jonroby <joncaroby@gmail.com>
2019-02-26 21:40:03 -0800

    npm i react router dom and add routes component

---
+++ src/Routes.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./App";
+
+class Routes extends Component {
+  render() {
+    return (
+      <BrowserRouter>
+        <React.Fragment>
+          <Switch>
+            <Route path="/search/movies" component={App} />
+          </Switch>
+        </React.Fragment>
+      </BrowserRouter>
+    );
+  }
+}
+
+export default Routes;
