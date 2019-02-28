ec34f7692317c2500134fe425a9b536b39033a10
jonroby <joncaroby@gmail.com>
2019-02-27 18:20:11 -0800

    related movies

--- src/MovieScreen.jsx
+++ src/MovieScreen.jsx
@@ -2,6 +2,7 @@ import React, { Component, useState, useEffect } from "react";
 import { Link } from "react-router-dom";
 import qs from "query-string";
 import axios from "axios";
+import Movies from "./Movies";
 
 import "./MovieScreen.scss";
 
@@ -20,46 +21,57 @@ const MovieScreen = props => {
   };
 
   const id = props.match.params.id;
-  console.log("movie ", movie);
   return (
-    <div className="movie-screen-container">
-      <div className="movie-screen-left" />
+    <div>
+      <div className="movie-screen-container">
+        <div className="movie-screen-left" />
 
-      <div className="movie-screen-movie">
-        <div className="image-container">
-          <img
-            src={`https://image.tmdb.org/t/p/w1000_and_h563_face/${
-              movie.backdrop_path
-            }`}
-          />
-          <div className="movie-screen-movie-overlay" />
+        <div className="movie-screen-movie">
+          <div className="image-container">
+            <img
+              src={`https://image.tmdb.org/t/p/w1000_and_h563_face/${
+                movie.backdrop_path
+              }`}
+            />
+            <div className="movie-screen-movie-overlay" />
+          </div>
+        </div>
+
+        <div className="movie-screen-details-container">
+          <div className="movie-screen-details">
+            <div id="movie-screen-details-title">{movie.title}</div>
+            <div id="tagline">
+              <i>{movie.tagline}</i>
+            </div>
+
+            <div className="movie-screen-numerical-data">
+              <div className="vote-average">{movie.vote_average}</div>
+              <div className="release-year">{movie.release_date}</div>
+              <div className="release-year">{movie.runtime}</div>
+            </div>
+            <div className="movie-screen-details-overview">
+              {movie.overview}
+            </div>
+            <div className="list-data">
+              <span className="label">Genres: </span>{" "}
+              {movie.genres && movie.genres.map(l => <div>{l.name}</div>)}
+            </div>
+
+            <div className="list-data">
+              <span className="label">Languages: </span>
+              {movie.genres &&
+                movie.spoken_languages.map(l => <div>{l.name}</div>)}
+            </div>
+          </div>
         </div>
       </div>
 
-      <div className="movie-screen-details-container">
-        <div className="movie-screen-details">
-          <div id="movie-screen-details-title">{movie.title}</div>
-          <div id="tagline">
-            <i>{movie.tagline}</i>
-          </div>
-
-          <div className="movie-screen-numerical-data">
-            <div className="vote-average">{movie.vote_average}</div>
-            <div className="release-year">{movie.release_date}</div>
-            <div className="release-year">{movie.runtime}</div>
-          </div>
-          <div className="movie-screen-details-overview">{movie.overview}</div>
-          <div className="list-data">
-            <span className="label">Genres: </span>{" "}
-            {movie.genres && movie.genres.map(l => <div>{l.name}</div>)}
-          </div>
-
-          <div className="list-data">
-            <span className="label">Languages: </span>
-            {movie.genres &&
-              movie.spoken_languages.map(l => <div>{l.name}</div>)}
-          </div>
-        </div>
+      <div className="movies-screen">
+        <h3>Related</h3>
+        <Movies
+          url={`${rootApiUrl}/movies/related/${id}`}
+          location={props.location}
+        />
       </div>
     </div>
   );
