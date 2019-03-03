bd260114551d09952a8761a1dc92b04b6602eebc
jonroby <joncaroby@gmail.com>
2019-03-03 11:45:22 -0800

    fix relative import for home screen

--- src/Components/Screens/HomeScreen.jsx
+++ src/Components/Screens/HomeScreen.jsx
@@ -1,8 +1,8 @@
 import React, { Component } from "react";
 import axios from "axios";
 import ListOfSliders from "../ListOfSliders";
-import formatFilmDuration from "../../helpers/formatFilmDuration";
-import extractYear from "../../helpers/extractYear";
+import formatFilmDuration from "../helpers/formatFilmDuration";
+import extractYear from "../helpers/extractYear";
 
 import "./HomeScreen.scss";
 
